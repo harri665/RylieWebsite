b43fa6665c4d3ca200d0fc2f6eb2a0ab9586af69
Harrison Martin <harrison.d.martin@gmail.com>
2023-06-14 08:00:02 -0600

    first

---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,6 @@
+export default function NavBar() { 
+
+    return(
+        <h1>Rlyie Website</h1>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NavBar from './Components/NavBar/Navbar'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <NavBar></NavBar>
+  </React.StrictMode>,
+)
