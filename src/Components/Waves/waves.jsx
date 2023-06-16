df9f2991aa1305906974b330a072156e9e492847
Harrison Martin <harrison.d.martin@gmail.com>
2023-06-16 12:37:29 -0600

    first

---
+++ src/Components/Waves/waves.jsx
@@ -0,0 +1,10 @@
+import "./waves.css"
+export default function Waves() { 
+
+    return(
+        <div class="ocean">
+            <div class="wave"></div>
+            <div class="wave"></div>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import NavBar from './Components/NavBar/Navbar'
+import Waves from './Components/Waves/waves'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <NavBar></NavBar>
+      <Waves></Waves>
   </React.StrictMode>,
 )
