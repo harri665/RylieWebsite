494880825505d856d30712e78c52106781dbfb9a
hari665 <harrison.d.martin@gmail.com>
2023-07-21 11:37:16 -0600

    save

---
+++ src/Components/Cards/Card.jsx
@@ -0,0 +1,9 @@
+import './card.css'
+
+export default function Card() { 
+    return (
+        <div className="card">
+            <img src='/src/Components/Cards/main.png'></img>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import NavBar from './Components/NavBar/Navbar'
 import Waves from './Components/Waves/waves'
+import Card from './Components/Cards/Card'
 import './index.css'
+// DEFAULT ** IMPORTANT ** 
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//       <NavBar></NavBar>
+//       <Waves></Waves>
+//   </React.StrictMode>,
+// )
 
+
+
+//Test 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <NavBar></NavBar>
-      <Waves></Waves>
+    <Card></Card>
   </React.StrictMode>,
 )
